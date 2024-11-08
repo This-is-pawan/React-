f94933280806a25010117caf68518b3020442bc6
pawan <pawanjalandhara@gmail.com>
2024-11-08 16:17:32 +0530

    reactnav

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Greeting() {
+  return (
+    <>
+     <Message/>
+    </>
+  );
+}
+
+
+
+function Message(){
+  return (
+    <>
+     <nav>
+        <div className="navBar">
+          <ul className="unOrderList">
+            <li><a href="&#">Home</a></li>
+            <li><a href="&#">About</a></li>
+            <li><a href="&#">Contact</a></li>
+            <li><a href="&#">FAQ</a></li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+
+function App() {
+  return (
+    <>
+      <Greeting />
+      
+    </>
+  );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
