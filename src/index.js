ab043eb3ad83e96ec712527127f689aa5f3652f8
pawan <pawanjalandhara@gmail.com>
2024-11-08 17:23:32 +0530

    booklist

--- src/index.js
+++ src/index.js
@@ -3,45 +3,48 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 
-function Greeting() {
+const BookList=()=> {
   return (
     <>
-     <Message/>
+     <Book/>
+     <Book/>
+     <Book/>
+     <Book/>
     </>
   );
 }
 
 
 
-function Message(){
+function Book(){
   return (
     <>
-     <nav>
-        <div className="navBar">
-          <ul className="unOrderList">
-            <li><a href="&#">Home</a></li>
-            <li><a href="&#">About</a></li>
-            <li><a href="&#">Contact</a></li>
-            <li><a href="&#">FAQ</a></li>
-          </ul>
-        </div>
-      </nav>
+    <div className="main">
+     <Image/>
+     <Title/>
+     <Author/>
+     </div>
     </>
   )
 }
 
 
-function App() {
+function Image() {
   return (
     <>
-      <Greeting />
-      
+    <img src="https://m.media-amazon.com/images/I/41POhI5EGaL._SY445_SX342_.jpg" alt="The Art of Moving On" title="The Art of Moving On" className="image" />
+    
+  
     </>
   );
 }
-
-
+const Title =()=> <h3>The Art of Moving On</h3>
+const Author =()=> (<><p>Author<b title="famous author">:Harshita Gupta</b></p>
+<article className="btnAlign">
+<button className="btn"><a href="https://www.forbesindia.com/lists/2023-digital-stars/profile/harshita-gupta/9">More About</a></button>
+</article>
+</>)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<BookList />);
 
 
